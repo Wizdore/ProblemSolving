397e17f5d0b727bf88136d54d6e3b8b175345633
nocmnt <shada.kaalo@gmail.com>
2015-06-30 20:35:57 +0600

    1585 - Score
    UvaOJ is down so technically its not tested...But seems alright.

--- main.cpp
+++ main.cpp
@@ -6,5 +6,26 @@ using namespace std;
 
 int main()
 {
-    //freopen("test.in","r",stdin);
+    freopen("test.in","r",stdin);
+
+    int n; cin>>n;
+    while(n--)
+    {
+        string s;
+        cin>>s;
+
+        int run = 0;
+        int score =0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] == 'O')
+            {
+                run++;
+                score+=run;
+            } else run =0;
+        }
+
+        cout<<score<<endl;
+    }
 }
